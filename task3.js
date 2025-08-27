fb1ec8a46095af7256f3d958125a8683e7284db8
mennaemamm <menna.emam.3333@gmail.com>
2025-08-27 23:47:08 +0300

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,14 @@
+var myName = 'menna';
+var gpa = 3.8;  
+
+function yourResult(name) {
+ console.log(`Welcome ${name} your gpa is ${gpa}`);
+    
+ if (gpa < 3) {
+        console.log('You have failed');
+ } else {
+        console.log('Congratulations');
+    }
+}
+
+yourResult('menna emamm'); 
